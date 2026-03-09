54a62eff5b791b0794a21cc972e15696acc4342f
duttachandan <duttaankit917@gmail.com>
2026-03-09 10:05:32 +0530

    updated intersection

--- src/intersection.ts
+++ src/intersection.ts
@@ -12,4 +12,11 @@ type likarChai = {
   tea: number;
 };
 
+type hotWater = Chai | specialChai;
+// here we are saying either have masala from Chai or have lemon from specialChai
+
+class water implements likarChai {
+  tea = 4;
+}
+// we can also use this type to implements in water class
 
