3514622c5a5e8557106e69f7a5fb78c70081e062
duttachandan <duttaankit917@gmail.com>
2026-03-09 10:25:21 +0530

    updated interfaces

--- src/class_interfaces.ts
+++ src/class_interfaces.ts
@@ -12,3 +12,17 @@ class lemonTea implements chaiGoods {
 
 // here we are implementing interfaces into the class means
 // we are creating a map that the class should follow and the property that the interface is having currently the class should have them too
+
+// we can also set readOnly values also in interfaces
+
+interface makeChai {
+  readonly sugar: number;
+  water: number;
+  tea: "3" | "4" | "5"; // this is also called as literals type
+}
+
+class chai implements makeChai {
+  sugar = 32;
+  water = 33;
+  tea: "3" | "4" | "5" = "3";
+}
