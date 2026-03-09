64a4802325d740e0f6bb12dee7e558edf673c6ae
duttachandan <duttaankit917@gmail.com>
2026-03-09 05:50:51 +0530

    updated second chapter of typescript

--- dist/index.js
+++ dist/index.js
@@ -31,4 +31,25 @@ const returnUnk = (param) => {
 };
 console.log(returnUnk(32));
 console.log(returnUnk("webskitters"));
+// returnUnk("chandan").toUpperCase();
+// so you can't use toUppercase as this is unknown,
+// whether if it was of "any" type then you can mention anything here-
+// -it will not going to throw any error over here but as we are working with the unknown type
+// so either you use this like this or like this
+returnUnk("chandan").toUpperCase(); // used Forcefull type assertion discussed below
+if (typeof returnAny("chandan") === "string") {
+    returnAny("chandan").toUppercase;
+    // in here it will not going to throw any error here
+}
+// ForceFull Type Assertion
+const data = "432";
+const typeAssertion = data.length;
+const mutatingLocalstorageLike = '{"name": "chandan"}';
+// obj are saved in the localstorage as string
+const parseObjectFromJson = JSON.parse(mutatingLocalstorageLike);
+// here we have used forcefull assertion
+console.log(parseObjectFromJson);
+// Another example with html element
+const text = document.querySelector(".text");
+console.log(text);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -44,4 +44,47 @@ const returnUnk = (param: unknown): unknown => {
 console.log(returnUnk(32));
 console.log(returnUnk("webskitters"));
 
+// returnUnk("chandan").toUpperCase();
+
+// so you can't use toUppercase as this is unknown,
+// whether if it was of "any" type then you can mention anything here-
+// -it will not going to throw any error over here but as we are working with the unknown type
+// so either you use this like this or like this
+
+(returnUnk("chandan") as string).toUpperCase(); // used Forcefull type assertion discussed below
+
+if (typeof returnAny("chandan") === "string") {
+  returnAny("chandan").toUppercase;
+  // in here it will not going to throw any error here
+}
+
+// ForceFull Type Assertion
+
+const data: string = "432";
+
+const typeAssertion: number = (data as string).length;
+
+// Forcefully I am telling the tsc that the data is a string so show us all the methods for string
+// if we don't use forcefull Type Assertion then we will not going to get the suggestion of all the methods
+// another example
+
+type book = {
+  name: string;
+};
+
+const mutatingLocalstorageLike = '{"name": "chandan"}';
+// obj are saved in the localstorage as string
+const parseObjectFromJson = JSON.parse(mutatingLocalstorageLike) as book;
+// here we have used forcefull assertion
+
+console.log(parseObjectFromJson);
+
+// Another example with html element
+
+const text = document.querySelector(".text") as HTMLElement;
+
+console.log(text);
+
+
+
 
