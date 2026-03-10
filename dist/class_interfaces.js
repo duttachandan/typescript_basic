293f519971d9af14366e886565ab21a076f16500
duttachandan <duttaankit917@gmail.com>
2026-03-10 10:40:31 +0530

    update typescript

--- dist/class_interfaces.js
+++ dist/class_interfaces.js
@@ -5,5 +5,47 @@ class lemonTea {
     water = 3;
     sugar = 44;
 }
-console.log(lemonTea.toString);
+class chai {
+    sugar = 32;
+    water = 33;
+    tea = "3";
+}
+const student = function (studentname, studentCourse) {
+    console.log(studentCourse, studentname);
+};
+// or
+const student2 = (studentname, studentCourse) => {
+    return studentname;
+};
+console.log(student2("chandan", "html"));
+// or
+function student3(studentName, coursePrice) {
+    return coursePrice;
+}
+console.log(student3("chandan", 6300));
+// union / literal types
+const value = "chandan"; // this is literall types
+// I have literally mentioned all the types of mine that can be allowed over here
+// but for union types I am declaring the type it can accept as value
+let chandan; // this is union type
+chandan = 32;
+chandan = "44";
+// chandan = true;  here you will see error because the accepted type is either number or string
+// declaring array types
+const arr = ["h", "a", "b"];
+// or
+const arr2 = ["h", "a", "b"];
+const arrNum = [3, 3, 2, 0, 4, 5];
+// or
+const arrNum2 = [3, 2, 3, 4, 5];
+const tupple = ["chandan", 3, 4, "chandan"];
+const tuppleNum = ["3", 3, 3, 3, 4, 4, 5]; // this rest tupples in here
+function sayTupple(value) {
+    return value[0];
+}
+// or
+const sayTupple2 = (value) => {
+    return value[0];
+};
+console.log(sayTupple2([3, 2, 3]));
 //# sourceMappingURL=class_interfaces.js.map
--- src/class_interfaces.ts
+++ src/class_interfaces.ts
@@ -26,3 +26,79 @@ class chai implements makeChai {
   water = 33;
   tea: "3" | "4" | "5" = "3";
 }
+
+// Interfaces for Functions
+
+interface makeStudent {
+  (strudentName: string, studentCourse: string): void;
+}
+
+const student: makeStudent = function (studentname, studentCourse) {
+  console.log(studentCourse, studentname);
+};
+
+// or
+
+const student2: makeStudent = (studentname, studentCourse) => {
+  return studentname;
+};
+
+console.log(student2("chandan", "html"));
+
+// or
+
+function student3(studentName: string, coursePrice: number): number {
+  return coursePrice;
+}
+
+console.log(student3("chandan", 6300));
+
+// union / literal types
+
+const value: "chandan" | "dutta" = "chandan"; // this is literall types
+// I have literally mentioned all the types of mine that can be allowed over here
+
+// but for union types I am declaring the type it can accept as value
+
+let chandan: string | number; // this is union type
+
+chandan = 32;
+chandan = "44";
+// chandan = true;  here you will see error because the accepted type is either number or string
+
+// declaring array types
+
+const arr: string[] = ["h", "a", "b"];
+// or
+const arr2: Array<string> = ["h", "a", "b"];
+
+const arrNum: number[] = [3, 3, 2, 0, 4, 5];
+// or
+const arrNum2: Array<number> = [3, 2, 3, 4, 5];
+
+const tupple: [string, number, number, string] = ["chandan", 3, 4, "chandan"];
+
+const tuppleNum: [string, number, ...number[]] = ["3", 3, 3, 3, 4, 4, 5]; // this rest tupples in here
+
+
+//Generic Types
+
+
+function sayTupple<T>(value: T[]): T | undefined {
+  return value[0];
+}
+
+// or
+
+const sayTupple2 = <T>(value: T[]): T | undefined => {
+  return value[0];
+};
+
+console.log(sayTupple2<number>([3, 2, 3])); 
+
+// Here I am mentioning the value that the function should accept
+// also return the same value this are helpfull for reusable component in react js
+
+
+
+
