07e6f6162eb0860131d816ad1c98a369df4a0181
duttachandan <duttaankit917@gmail.com>
2026-03-12 07:35:08 +0530

    updated generics in a more simplear version

--- src/class_interfaces.ts
+++ src/class_interfaces.ts
@@ -80,6 +80,20 @@ const tupple: [string, number, number, string] = ["chandan", 3, 4, "chandan"];
 
 const tuppleNum: [string, number, ...number[]] = ["3", 3, 3, 3, 4, 4, 5]; // this rest tupples in here
 
+enum chaiOrCode {
+  SMALL = 101, // this value assignation is not mandatory but if would do the next enum property will increase the value by default
+  LARGE, // after increase the value will be set to LARGE = 102, EXTRALARGE = 103
+  EXTRALARGE,
+}
+
+// enum is basically stricting the user, here by declaring a enum I have simply said if you'd want to use this enum
+// then you will have to select between this values only
+// values are put in Capital Letter as this is the best practice
+
+const cupSize = chaiOrCode.EXTRALARGE;
+
+console.log(cupSize);
+
 //Generic Types
 
 function sayTupple<T>(value: T[]): T | undefined {
@@ -93,21 +107,18 @@ const sayTupple2 = <T>(value: T[]): T | undefined => {
 };
 
 console.log(sayTupple2<number>([3, 2, 3]));
+
 // Here I am mentioning the value that the function should accept
 // also return the same value this are helpfull for reusable component in react js
 
-
-
-
-
-// here I am making a type from which we are using partial as generics 
+// here I am making a type from which we are using partial as generics
 // so what will happen when we will call that function that function requires any of those water/coffe
 // from that makeCoffe type benifits of using partial
+
 type makeCoffe = {
   water: string;
   coffe: string;
 };
-
 
 const mochaCofee = (update: Partial<makeCoffe>) => {
   return update;
@@ -116,3 +127,43 @@ const mochaCofee = (update: Partial<makeCoffe>) => {
 console.log(mochaCofee({ water: "32ml" }));
 console.log(mochaCofee({ coffe: "32ml", water: "40ml" }));
 
+// here I am using Required as generics so this will make all the property in type as required whether you have declared them as
+// optional in type declaration
+
+type coffeBasic = {
+  coffee?: string; // by putting that ? we are making it optional
+  water?: string;
+  sugar?: number;
+};
+
+const CofeeMacroni = (update: Required<coffeBasic>) => {
+  return update;
+};
+
+console.log(CofeeMacroni({ coffee: "32gm", water: "100ml", sugar: 20 }));
+// here I have mentioned eveything in this
+
+// there is other two ways to declare generics pick/omit
+
+type coffeNew = Pick<coffeBasic, "coffee" | "water">;
+// so here I am picking from anther type coffeBasic and also I have mentioned which properties we wants to pick in here
+
+const makeNewCoffee = (value: coffeNew) => {
+  return value;
+};
+
+console.log(makeNewCoffee({ coffee: "40gm", water: "300ml" }));
+
+// now we will use omit by omit we will declare which are those properties that we don't want to
+// inherit from an existing type
+
+type coffeeOld = Omit<coffeBasic, "water" | "coffee">;
+
+// here I have said we are not going use water and coffee from coffeBasic so the coffeOld takes only one value
+// which is sugar
+
+const makeOldCoffee = (value: coffeeOld) => {
+  return value;
+};
+
+console.log(makeOldCoffee({ sugar: 30 }));
